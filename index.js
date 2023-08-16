1c870c079c759c0f16f9f2dbd04af9e1f8f89a50
Raul Calvo <raul.cal.bar@gmail.com>
2023-08-16 20:08:35 +0200

    added basic express server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const server = express()
+
+server.get('/', (req,res) => {
+  res.send('<h3> Server is running </h3>')
+})
+
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT, () => console.log(`listening on port ${PORT}`))
+
