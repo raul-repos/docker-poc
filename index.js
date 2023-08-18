c16aaac0d8154d2a5d18e534c4d435ba3144f3ef
Raul Calvo <raul.cal.bar@gmail.com>
2023-08-18 19:25:48 +0200

    added mongoose

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const server = express()
+
+mongoose.connect("mongodb://usertest:passtest@mongo:27017/?authSource=admin")
+.then(() => console.log('connection with DB: success'))
+.catch(e => console.log(e))
 
 server.get('/', (req,res) => {
   res.send('<h3> Server is running!!</h3>')
