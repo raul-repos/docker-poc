877e2b564148a99172519d9c9e899bdc94e3e69d
Raul Calvo <raul.cal.bar@gmail.com>
2023-08-18 19:38:09 +0200

    organize env variables

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT, PORT } = require('./config/config')
 
 const server = express()
 
-mongoose.connect("mongodb://usertest:passtest@mongo:27017/?authSource=admin")
+const MONGO_URL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+
+mongoose.connect(MONGO_URL)
 .then(() => console.log('connection with DB: success'))
 .catch(e => console.log(e))
 
 server.get('/', (req,res) => {
   res.send('<h3> Server is running!!</h3>')
 })
-
-const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () => console.log(`listening on port ${PORT}`))
 
